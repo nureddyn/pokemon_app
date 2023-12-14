c53e880fcb2bb7221df7f0f6777c5d687b5f0215
nureddyn <nureddynballout@gmail.com>
2023-12-14 15:40:54 -0300

    I can view a list of all my pokemon in the browser

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.engine("jsx", require("express-react-views").createEngine());
 
 
 app.get('/', (req, res) => {
-    res.render('Index');
+    res.render('Index', {pokemons: pokemon});
 });
 
 app.get('/pokemon', (req, res) => {
--- views/Index.jsx
+++ views/Index.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import styles, { index, indexHeader } from './styles';
 
-export default function Index() {
+export default function Index({ pokemons }) {
   return (
     <div style={index}>
       <h1 style={indexHeader}>Pokemon Page</h1>
+      <ul>
+        {pokemons.map((pokemon, i) => {
+          return (
+            <li key={i}>
+              <p>
+                {`${pokemon.name.charAt(0).toUpperCase()}${pokemon.name.slice(1)}`}
+              </p>
+            </li>
+          )
+        })}
+      </ul>
     </div>
   )
 }
